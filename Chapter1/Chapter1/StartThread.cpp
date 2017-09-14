cca103118198e0f7473ecc207674ee1c1150c01d
40208539 <40208539@live.napier.ac.uk>
2017-09-14 11:19:02 +0100

    finished exercise 1

--- Chapter1/Chapter1/StartThread.cpp
+++ Chapter1/Chapter1/StartThread.cpp
@@ -2,14 +2,23 @@
 #include <iostream>
 #include <thread>
 
+using namespace std;
+
+/*
+This is the method called by the thread.
+*/
 void hello_world()
 {
-	std::cout << "Hello World!" << std::endl;
+	cout << "Hello World! Thread : " << this_thread::get_id() << endl;
 }
 
 int main()
 {
-	std::thread t(hello_world);
+	// Create a new thread
+	thread t(hello_world);
+	// Wait for thread to finish (join it)
+	t.join();
+	// Return OK (0)
     return 0;
 }
 
