e5039182484f188a96fa6951e9ce70530301a39c
Jack Anderson <40208539@live.napier.ac.uk>
2017-10-12 06:56:09 +0100

    fixed crashing in raytracer (now blank)

--- Coursework/Raytracer/Raytracer.cpp
+++ Coursework/Raytracer/Raytracer.cpp
@@ -34,63 +34,95 @@ constexpr const T &clamp(const T &v, const T &lo, const T &hi)
 
 struct vec
 {
-	__m128 *components;
+	__m128 components;
 
 	//Working.
-	vec(float x = 0, float y = 0, float z = 0) noexcept
+	vec(float x = 0.0f, float y = 0.0f, float z = 0.0f) noexcept
 	{
-		components = (__m128*)_aligned_malloc(4 * sizeof(float), 16);
-		*components = _mm_setr_ps(x, y, z, 0.0f);
+		//components = *(__m128*)_aligned_malloc(sizeof(float) * 4, 16);
+		components = _mm_set_ps(0.0f, z, y, x);
+		/*
+		components.m128_f32[0] = x;
+		components.m128_f32[1] = y;
+		components.m128_f32[2] = z;
+		components.m128_f32[3] = 0.0f;
+		*/
 	}
 
 	//Working.
 	vec(__m128 vector) noexcept
 	{
-		components = (__m128*)_aligned_malloc(4 * sizeof(float), 16);
-		*components = vector;
+		//components = *(__m128*)_aligned_malloc(sizeof(float) * 4, 16);
+		components = vector;
 	}
-	
-	//Should Work.
-	~vec()
+
+	//vec(const vec& copy)
+	//{
+		//components = *(__m128*)_aligned_malloc(sizeof(float) * 4, 16);
+	//	components = copy.components;
+	//}
+
+	float get_x() noexcept
 	{
-		_aligned_free(components);
+		return components.m128_f32[0];
+	}
+
+	float get_y() noexcept
+	{
+		return components.m128_f32[1];
+	}
+
+	float get_z() noexcept
+	{
+		return components.m128_f32[2];
 	}
 
 	//Working.
-	vec operator+(const vec &other) const noexcept
+	vec operator+(const vec other) const noexcept
 	{
-		return vec(_mm_add_ps(*other.components, *components));
+		return vec(_mm_add_ps(other.components, components));
 	}
 
-	vec operator-(const vec &other) const noexcept
+	vec operator-(const vec other) const noexcept
 	{
-		return vec(_mm_sub_ps(*other.components, *components));
+		return vec(_mm_sub_ps(other.components, components));
 	}
 
-	vec operator*(double scale) const noexcept
+	vec operator*(float scale) const noexcept
 	{
-		return vec(_mm_mul_ps(*components, _mm_set1_ps(scale)));
+		return vec(_mm_mul_ps(components, _mm_set1_ps(scale)));
 	}
 
 	vec mult(const vec &other) const noexcept
 	{
-		return vec(_mm_mul_ps(*other.components, *components));
+		return vec(_mm_mul_ps(other.components, components));
 	}
 
 	vec normal() const noexcept
 	{
-		return *this * (1.0 / sqrt(x * x + y * y + z * z));
+		// Square each component
+		__m128 result = _mm_mul_ps(components, components);
+		// Sum all components
+		result.m128_f32[0] = result.m128_f32[1] = result.m128_f32[2] = result.m128_f32[0] + result.m128_f32[1] + result.m128_f32[2];
+		// Find reciprocal squrare root and times by original vector
+		return vec(_mm_mul_ps(_mm_rsqrt_ps(result), components));
 	}
 
-	double dot(const vec &other) const noexcept
+	float dot(const vec &other) const noexcept
 	{
-		return x * other.x + y * other.y + z * other.z;
+		__m128 result = _mm_mul_ps(other.components, components);
+		return result.m128_f32[0] + result.m128_f32[1] + result.m128_f32[2];
 	}
 
 	vec cross(const vec &other) const noexcept
 	{
-		return vec(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+		return vec(components.m128_f32[1] * other.components.m128_f32[2] - components.m128_f32[2] * other.components.m128_f32[1], components.m128_f32[2] * other.components.m128_f32[0] - components.m128_f32[0] * other.components.m128_f32[2], components.m128_f32[0] * other.components.m128_f32[1] - components.m128_f32[1] * other.components.m128_f32[0]);
+		//return vec(_mm_sub_ps(
+		//	_mm_mul_ps(_mm_shuffle_ps(*other.components, *other.components, _MM_SHUFFLE(3, 0, 2, 1)), _mm_shuffle_ps(*components, *components, _MM_SHUFFLE(3, 1, 0, 2))),
+		//	_mm_mul_ps(_mm_shuffle_ps(*other.components, *other.components, _MM_SHUFFLE(3, 1, 0, 2)), _mm_shuffle_ps(*components, *components, _MM_SHUFFLE(3, 0, 2, 1)))
+		//));
 	}
+
 };
 
 struct ray
@@ -112,7 +144,7 @@ struct sphere
 	vec emission, colour;
 	reflection_type reflection;
 
-	sphere(double radius, const vec &position, const vec &emission, const vec &colour, reflection_type reflection) noexcept
+	sphere(double radius, vec position, vec emission, vec colour, reflection_type reflection) noexcept
 		: radius(radius), position(position), emission(emission), colour(colour), reflection(reflection)
 	{
 	}
@@ -183,7 +215,13 @@ vec radiance(const vector<sphere> &spheres, const ray &the_ray, int depth) noexc
 	vec intersection_normal = (hit_point - hit_sphere.position).normal();
 	vec pos_intersection_normal = intersection_normal.dot(the_ray.direction) < 0 ? intersection_normal : intersection_normal * -1;
 	vec colour = hit_sphere.colour;
-	double max_reflection = max({ colour.x, colour.y, colour.z });
+	double max_reflection = max({ colour.get_x(), colour.get_y(), colour.get_z() });
+
+	if (sphere_index == 1)
+	{
+		cout << "" << endl;
+	}
+
 	if (depth > MAX_DEPTH)
 	{
 		return hit_sphere.emission;
@@ -205,7 +243,7 @@ vec radiance(const vector<sphere> &spheres, const ray &the_ray, int depth) noexc
 		double r1 = 2.0 * PI * get_random_number();
 		double r2 = get_random_number();
 		vec w = pos_intersection_normal;
-		vec u = ((abs(w.x) > 0.1 ? vec(0, 1, 0) : vec(1, 0, 0)).cross(w)).normal();
+		vec u = ((abs(w.get_x()) > 0.1 ? vec(0, 1, 0) : vec(1, 0, 0)).cross(w)).normal();
 		vec v = w.cross(u);
 		vec new_direction = (u * cos(r1) * sqrt(r2) + v * sin(r1) * sqrt(r2) + w * sqrt(1 - r2)).normal();
 		return hit_sphere.emission + colour.mult(radiance(spheres, ray(hit_point, new_direction), depth));
@@ -299,7 +337,7 @@ bool array2bmp(const std::string &filename, const vector<vec> &pixels, const std
 		for (std::size_t y = 0; y < width; y++)
 		{
 			const auto &val = pixels[((x - 1) * width) + y];
-			f.put(static_cast<char>(int(255.0 * val.z))).put(static_cast<char>(int(255.0 * val.y))).put(static_cast<char>(255.0 * val.x));
+			f.put(static_cast<char>(int(255.0f * val.components.m128_f32[2]))).put(static_cast<char>(int(255.0f * val.components.m128_f32[1]))).put(static_cast<char>(255.0f * val.components.m128_f32[0]));
 		}
 		if (padding_size)
 		{
@@ -311,6 +349,40 @@ bool array2bmp(const std::string &filename, const vector<vec> &pixels, const std
 
 int main(int argc, char **argv)
 {
+	/*
+		--- TESTING ---
+	*/
+
+	vec a = vec(1.0, 2.0, 1.0);
+	vec b = vec(2.0, 2.0, 1.0);
+
+	vec c = a.mult(b);
+
+	if (c.get_x() != 2.0f || c.get_y() != 4.0f || c.get_z() != 1.0f)
+		throw new exception;
+
+	float dot = a.dot(b);
+
+	if (dot != 7.0f)
+		throw new exception;
+
+	vec cross = a.cross(b);
+
+	if (cross.get_x() != 0 || cross.get_y() != 1.0f || cross.get_z() != -2.0f)
+		throw new exception;
+
+	float clampa = clamp(0.002f, 0.0f, 1.0f); //0.002
+	float clampb = clamp(-10.0f, 0.0f, 1.0f); //0.0
+	float clampc = clamp(12.2f, 0.0f, 1.0f); // 1.0
+
+	if (clampa != 0.002f || clampb != 0.0f || clampc != 1.0f)
+		throw new exception;
+
+	/*
+		-- END TESTING ---
+	*/
+
+
 	random_device rd;
 	default_random_engine generator(rd());
 	uniform_real_distribution<double> distribution;
@@ -321,8 +393,9 @@ int main(int argc, char **argv)
 	constexpr std::size_t samples = 2; // Algorithm performs 4 * samples per pixel.
 	vector<sphere> spheres
 	{
-		sphere(1e5, vec(1e5 + 1, 40.8, 81.6), vec(), vec(0.75, 0.25, 0.25), reflection_type::DIFFUSE),
-		sphere(1e5, vec(-1e5 + 99, 40.8, 81.6), vec(), vec(0.25, 0.25, 0.75), reflection_type::DIFFUSE),
+		// ******** !!!!!!!!! COLOR IS BEING SET TO 0,0,0 IN THE RADIANCE FUNCTION FIX IT !!!!!!!!!!!!************
+		sphere(1e5, vec(1e5f + 1.0f, 40.8f, 81.6f), vec(0.0f, 0.0f, 0.0f), vec(0.75f, 0.25f, 0.25f), reflection_type::DIFFUSE),
+		sphere(1e5, vec(-1e5f + 99.0f, 40.8f, 81.6f), vec(), vec(0.25f, 0.25f, 0.75f), reflection_type::DIFFUSE),
 		sphere(1e5, vec(50, 40.8, 1e5), vec(), vec(0.75, 0.75, 0.75), reflection_type::DIFFUSE),
 		sphere(1e5, vec(50, 40.8, -1e5 + 170), vec(), vec(), reflection_type::DIFFUSE),
 		sphere(1e5, vec(50, 1e5, 81.6), vec(), vec(0.75, 0.75, 0.75), reflection_type::DIFFUSE),
@@ -334,16 +407,16 @@ int main(int argc, char **argv)
 	// **************************************************************************************
 
 	ray camera(vec(50.0f, 52.0f, 295.6f), vec(0.0f, -0.042612f, -1.0f).normal());
-	vec cx = vec(0.5135);
+	vec cx = vec(0.5135f);
 	vec cy = (cx.cross(camera.direction)).normal() * 0.5135;
-	vec r;
+	vec r = vec();
 	vector<vec> pixels(dimension * dimension);
 
 	unsigned int NUM_THREADS = thread::hardware_concurrency();
 	auto start = system_clock::now();
 
 	//For each row of pixels.
-#pragma omp parallel for num_threads(NUM_THREADS) shared(pixels) private(r)
+//#pragma omp parallel for num_threads(NUM_THREADS) shared(pixels) private(r)
 	for (int y = 0; y < dimension; ++y)
 	{
 		std::cout << "Rendering " << dimension << " * " << dimension << "pixels. Samples:" << samples * 4 << " spp (" << 100.0 * y / (dimension - 1) << ")" << endl;
@@ -365,11 +438,19 @@ int main(int argc, char **argv)
 						double dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
 
 						vec direction = cx * static_cast<double>(((sx + 0.5 + dx) / 2 + x) / dimension - 0.5) + cy * static_cast<double>(((sy + 0.5 + dy) / 2 + y) / dimension - 0.5) + camera.direction;
+						
+						ray rayman = ray(camera.origin + direction * 140, direction.normal());
+						vec radiance1 = radiance(spheres, ray(camera.origin + direction * 140, direction.normal()), 0);
+						vec radiance2 = radiance1 * (1.0 / samples);
 
 						r = r + radiance(spheres, ray(camera.origin + direction * 140, direction.normal()), 0) * (1.0 / samples);
+
+						if (r.get_x() != 0.0f)
+							throw new exception;
 					}
 
-					pixels[i] = pixels[i] + vec(clamp(r.x, 0.0, 1.0), clamp(r.y, 0.0, 1.0), clamp(r.z, 0.0, 1.0)) * 0.25;
+					pixels[i] = pixels[i] + vec(clamp(r.get_x(), 0.0f, 1.0f), clamp(r.get_y(), 0.0f, 1.0f), clamp(r.get_z(), 0.0f, 1.0f)) * 0.25f;
+
 				}
 			}
 		}
